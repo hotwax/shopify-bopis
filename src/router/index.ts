57fe0903e00c85fbe5c36bacaf697e428b3c54b5
Disha Talreja <dishatalreja1202@gmail.com>
2022-07-11 17:59:35 +0530

    Implemented login flow(#2jueprc)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,44 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Install from '@/views/Install.vue'
 import Home from '@/views/Home.vue'
+import Settings from "@/views/Settings.vue"
+import Login from '@/views/Login.vue'
+import store from '@/store'
+
+const authGuard = (to: any, from: any, next: any) => {
+  if (store.getters['user/isAuthenticated']) {
+    next()
+  } else {
+    next("/login")
+  }
+};
+
+const loginGuard = (to: any, from: any, next: any) => {
+  if (!store.getters['user/isAuthenticated']) {
+    next()
+  } else {
+    next("/")
+  }
+};
+
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/install',
     name: 'Install',
     component: Install
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter: loginGuard
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+    beforeEnter: authGuard
   },
   {
     path: '/',
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,7 @@ import actions from './actions'
 import RootState from './RootState'
 import shopModule from "./modules/shop"
 import createPersistedState from "vuex-persistedstate";
+import userModule from "./modules/user"
 
 
 // TODO check how to register it from the components only
@@ -17,7 +18,7 @@ const state: any = {
 }
 
 const persistState = createPersistedState({
-    paths: [],
+    paths: ['user'],
     fetchBeforeUse: true
 })
 
@@ -29,7 +30,8 @@ const store = createStore<RootState>({
     getters,
     plugins: [ persistState ],
     modules: {
-        'shop': shopModule
+        'shop': shopModule,
+        'user': userModule
     }
 })
 
